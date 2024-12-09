3f2cc687ed45618eafc093a6559878c40371a852
Sharik3k <vladyslav.senkiv@gmail.com>
2024-12-09 19:44:31 +0200

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+const paymentData = JSON.parse(localStorage.getItem("paymentData")) || {
+    
+}
     document.getElementById("payment-form").addEventListener("submit", (event) => {
         event.preventDefault();
         alert(`Оплата ${paymentData.name} на суму ${paymentData.price} грн успішно виконана!`);
