a0384ce0cdc3edba25d04295ae28cce9e5b971f1
Sharik3k <vladyslav.senkiv@gmail.com>
2024-12-09 19:46:36 +0200

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 const paymentData = JSON.parse(localStorage.getItem("paymentData")) || {
-    
+name: "iPhone 16",
+name: "iPhone 15 Pro",
+name: "Samsung Galaxy A25",
+name: "Samsung Galaxy A35", 
+price: "1312313"
 }
     document.getElementById("payment-form").addEventListener("submit", (event) => {
         event.preventDefault();
